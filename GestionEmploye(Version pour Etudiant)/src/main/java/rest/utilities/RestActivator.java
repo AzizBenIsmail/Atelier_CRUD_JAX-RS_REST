f053771c3154e6ca0b7a7031bbbad4181e1cb499
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-10-10 12:39:03 +0100

    Version final

---
+++ GestionEmploye(Version pour Etudiant)/src/main/java/rest/utilities/RestActivator.java
@@ -0,0 +1,9 @@
+package rest.utilities;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("Gestion")
+public class RestActivator extends Application {
+
+}
